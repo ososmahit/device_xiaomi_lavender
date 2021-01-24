0a62830cf6cfb3fbceffe6fd517fb92c4b0d1d1c
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-01-24 18:23:30 +0700

    Update cherish_lavender.mk

--- cherish_lavender.mk
+++ cherish_lavender.mk
@@ -43,7 +43,11 @@ CHERISH_WITHGAPPS=true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys" 
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys"
+    BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 # Device identifier
 PRODUCT_NAME := cherish_lavender
